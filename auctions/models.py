b94b4e1be72509b60879da4efbd8f92c8ad27d04
dstansbury <davidmstansbury@gmail.com>
2022-04-28 08:22:56 -0400

    adds Comment and Bid models and makes updates to Listing to include user foreign key

--- auctions/models.py
+++ auctions/models.py
@@ -8,28 +8,37 @@ class User(AbstractUser):
 
 #Define the listing model
 class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
     image = models.URLField()
     # condition field intended to hold items
     # from a drop down with 4 options
     # new, excellent, good, as described
     condition = models.CharField(max_length=12)
+    # condition field intended to hold 
+    # user-created items from a drop down
+    category = models.CharField(max_length=64)
     description = models.TextField()
-    startPrice = models.DecimalField(decimal_places=2, max_digits=4)
-    currentPrice = models.DecimalField(decimal_places=2, max_digits=4)
+    startPrice = models.DecimalField(decimal_places=2, max_digits=7)
+    currentPrice = models.DecimalField(decimal_places=2, max_digits=7)
     # duration field intended to hold items
     # from a drop down with 3 options 
     # 1 day, 7 days, 10 days
     duration = models.CharField(max_length=12) 
     remainingTime = models.DurationField()
-    listDate = models.DateTimeField()
+    dateTime = models.DateTimeField()
 
     def __str__(self):
         return f"Auction {self.id}: {self.title}, condition {self.condition}"
 
+class Bid(modles.Model):
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2, max_digits=7)
+    dateTime= models.DateTimeField()
 
-
-
-#class Bid
-
-#class Comment
+class Comment:
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    dateTime = models.DateTimeField()
